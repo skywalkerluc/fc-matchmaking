0ae9a2796eb2208f1753dbf81aa47d9642adde27
Lucas Henrique Bezerra Ferreira <lucas.ferreira@lambda3.com.br>
2018-10-02 12:13:18 -0300

    Adding null value handling

--- Matchmaking.Engine/Matchmaking.Engine/Startup.cs
+++ Matchmaking.Engine/Matchmaking.Engine/Startup.cs
@@ -25,7 +25,14 @@ namespace Matchmaking.Engine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opcoes =>
+                {
+                    opcoes.SerializerSettings.NullValueHandling =
+                        Newtonsoft.Json.NullValueHandling.Ignore;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
